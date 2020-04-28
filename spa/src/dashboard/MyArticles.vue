40fdd32f0503e929f34c4e1939fcc1fe4cb066de
houssam boumaraf <houssambat@gmail.com>
2020-04-28 10:02:36 +0100

    feat: Added delete & update function to MyArticles.vue

--- spa/src/dashboard/MyArticles.vue
+++ spa/src/dashboard/MyArticles.vue
@@ -1,12 +1,54 @@
 <template>
   <div>
-    <h1>My Articles</h1>
+    <articles
+      :myArticles="myArticles"
+      v-on:deleteArticle="deleteArticle"
+      v-on:updateArticle="updateMyArticle"
+    />
   </div>
 </template>
 
 <script>
+    import axios from "axios";
+    import Articles from "../components/Articles";
+    import {apiUrl} from '@/variables.js'
+
     export default {
-        name: "MyArticles"
+        name: "MyArticles",
+        components: {
+            Articles
+        },
+        data (){
+            return {
+                myArticles:[],
+            }
+        },
+        methods:{
+            deleteArticle(id){
+                axios.post(`${apiUrl}/api/articles/delete/${id}`)
+                    .then(result => this.myArticles = result.data,
+                        this.successMessage = "Article Has Been Deleted Successfully",
+                        this.snackbar = true )
+                    .catch(error => console.log(error) );
+            },
+            updateMyArticle(id, updatedArticle){
+                let { title, body} = updatedArticle
+
+                axios.post(`${apiUrl}/api/articles/update/${id}`, {
+                    title,
+                    body
+                })
+                    .then(result => this.myArticles = result.data,
+                        this.successMessage = "Article Has Been Updated Successfully",
+                        this.snackbar = true )
+                    .catch(error => console.log(error) );
+            }
+        },
+        created(){
+            axios.get(apiUrl+'/api/myarticles')
+                .then(result => this.myArticles = result.data)
+                .catch(error => console.log(error))
+        },
     }
 </script>
 
