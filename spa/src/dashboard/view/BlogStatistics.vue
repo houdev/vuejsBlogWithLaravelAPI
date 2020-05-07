0394fbba4663ba2554a42b08c92ba8c4eb232639
houssam boumaraf <houssambat@gmail.com>
2020-05-07 14:21:41 +0100

    feat: Added some statistics to the dashboard's home page

---
+++ spa/src/dashboard/view/BlogStatistics.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="4">
+        <v-card color="#039BE5" dark elevation="5">
+          <v-card-title>
+            Total Articles
+          </v-card-title>
+          <v-card-subtitle class="display-2 mt-5">
+            {{ articles.length }}
+          </v-card-subtitle>
+        </v-card>
+      </v-col>
+      <v-col cols="4">
+        <v-card color="#0097A7" dark elevation="5">
+          <v-card-title>
+            My Articles
+          </v-card-title>
+          <v-card-subtitle class="display-2 mt-5">
+            {{ myArticles.length }}
+          </v-card-subtitle>
+        </v-card>
+      </v-col>
+      <v-col cols="4">
+        <v-card color="#00796B" dark elevation="5">
+          <v-card-title>
+            Total Members
+          </v-card-title>
+          <v-card-subtitle class="display-2 mt-5">
+            {{ members.length }}
+          </v-card-subtitle>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: "BlogStatistics",
+    props:{
+      articles: Array,
+      members: Array,
+      myArticles: Array,
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
--- spa/src/dashboard/view/dashboard.vue
+++ spa/src/dashboard/view/dashboard.vue
@@ -1,10 +1,44 @@
 <template>
- <h1>dashboard</h1>
+  <div>
+    <blog-statistics
+      :articles="articles"
+      :members="members"
+      :myArticles="myArticles"
+    />
+  </div>
 </template>
 
 <script>
+  import BlogStatistics from "./BlogStatistics";
+  import {apiUrl} from "../../variables";
+  import axios from "axios";
+
   export default {
-    name: "dashboard"
+    name: "dashboard",
+    components:{
+      BlogStatistics
+    },
+    data(){
+      return{
+        articles:[],
+        myArticles:[],
+        members:[],
+      }
+    },
+    created() {
+      axios.get(`${apiUrl}/api/articles`)
+      .then( result => this.articles = result.data)
+      .catch( error => console.log(error) );
+
+      axios.get(`${apiUrl}/api/myarticles`)
+        .then( result => this.myArticles = result.data)
+        .catch( error => console.log(error) );
+
+      axios.post(`${apiUrl}/api/users`)
+      .then( result => this.members = result.data)
+      .catch( error => console.log(error) );
+
+    }
   }
 </script>
 
