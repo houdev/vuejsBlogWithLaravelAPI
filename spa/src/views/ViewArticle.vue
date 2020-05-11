07bd1e814c037f2d0e2bcf4ebff0e3a2dd1f0a70
houssam boumaraf <houssambat@gmail.com>
2020-05-11 11:48:56 +0100

    style: Added some design to View article page

--- spa/src/views/ViewArticle.vue
+++ spa/src/views/ViewArticle.vue
@@ -1,24 +1,39 @@
 <template>
   <div class="about">
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <v-toolbar-title>Simple Blog</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        color="#00796B"
+        fab
+        dark
+        @click="$router.go(-1)"
+      >
+        <v-icon>mdi-arrow-left-bold</v-icon>
+      </v-btn>
+
+    </v-app-bar>
 
     <v-container>
         <v-row justify="center" align="center">
           <v-card width="85%">
 
-            <v-list-item >
+            <v-list-item class="text-center">
               <v-list-item-content>
                 <div class="overline mb-4">{{ myArticle.created_at }}</div>
                 <v-list-item-title class="headline mb-1">
                   {{ myArticle.title }}
                 </v-list-item-title>
 
-                <v-spacer></v-spacer>
-
                 <v-divider class="mx-4"></v-divider>
 
-                <v-spacer></v-spacer>
-
-                <v-list-item-text>{{ myArticle.body }}</v-list-item-text>
+                <p class="mt-5">{{ myArticle.body }}</p>
               </v-list-item-content>
             </v-list-item>
 
