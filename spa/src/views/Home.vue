d2ca36b80e083facb37252f1d699093998f8e389
houssam boumaraf <houssambat@gmail.com>
2020-05-12 14:55:07 +0100

    style: renamed route path

--- spa/src/views/Home.vue
+++ spa/src/views/Home.vue
@@ -26,7 +26,7 @@
           </template>
 
           <v-list>
-            <v-list-item to="/dashboard">
+            <v-list-item to="/dashboard/dashboard">
               <v-list-item-title>Dashboard</v-list-item-title>
             </v-list-item>
             <v-list-item @click="logout">
