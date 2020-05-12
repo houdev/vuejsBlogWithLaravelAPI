118f35762a16a3ec7a15c6982249d65b82917648
houssam boumaraf <houssambat@gmail.com>
2020-05-12 14:37:45 +0100

    feat: Added validation to the 'Add New Member' form (in Members page)

--- spa/src/dashboard/members/Add.vue
+++ spa/src/dashboard/members/Add.vue
@@ -14,18 +14,18 @@
       <template>
         <v-hover v-slot:default="{ hover }">
           <v-avatar
-                  class="avatar"
-                  cursor="pointer"
-                  width="100px"
-                  height="100px"
-                  @click="$refs.inputUpload.click()"
+            class="avatar"
+            cursor="pointer"
+            width="100px"
+            height="100px"
+            @click="$refs.inputUpload.click()"
           >
             <v-img src="https://cdn.vuetifyjs.com/images/john.jpg">
               <v-expand-transition>
                 <v-card
-                        v-if="hover"
-                        class="d-flex transition-fast-in-fast-out grey darken-1 v-card--reveal display-3"
-                        style="height: 50%;"
+                  v-if="hover"
+                  class="d-flex transition-fast-in-fast-out grey darken-1 v-card--reveal display-3"
+                  style="height: 50%;"
                 >
                   <v-icon>mdi-camera</v-icon>
                 </v-card>
@@ -38,33 +38,57 @@
       </template>
 
       <v-card-text>
-        <v-container>
-          <v-row>
-            <v-col cols="12" sm="6" md="6">
-              <v-text-field label="Full Name" v-model="name" required></v-text-field>
-            </v-col>
-            <v-col cols="12" sm="6" md="6">
-              <v-text-field label="E-mail" v-model="email" required></v-text-field>
-            </v-col>
-            <v-col cols="12" sm="6" md="6">
-              <v-text-field label="Password" type="password" v-model="pass" required></v-text-field>
-            </v-col>
-            <v-col cols="12" sm="6" md="6">
-              <v-select
-                :items="roles"
-                label="Role"
-                v-model="role"
-                required
-              ></v-select>
-            </v-col>
-          </v-row>
-        </v-container>
+        <v-form ref="addMemberForm">
+          <v-container>
+            <v-row>
+              <v-col cols="12" sm="6" md="6">
+                <v-text-field
+                  label="Full Name"
+                  v-model="name"
+                  required
+                  :rules="memberNameRules"
+                >
+                </v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="6">
+                <v-text-field
+                  label="E-mail"
+                  v-model="email"
+                  required
+                  :rules="memberEmailRules"
+                >
+                </v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="6">
+                <v-text-field
+                  label="Password" t
+                  ype="password"
+                  v-model="pass"
+                  required
+                  :rules="memberPasswordRules"
+                >
+                </v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="6">
+                <v-select
+                  :items="roles"
+                  label="Role"
+                  v-model="role"
+                  required
+                  :rules="memberRoleRules"
+                ></v-select>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-form>
       </v-card-text>
+
       <v-card-actions>
         <div class="flex-grow-1"></div>
         <v-btn color="red" dark  @click="dialog = false">Close</v-btn>
         <v-btn color="success"  @click="AddMember">Add Member</v-btn>
       </v-card-actions>
+
     </v-card>
   </v-dialog>
 </template>
@@ -80,10 +104,28 @@
                 pass: null,
                 roles: ['Author','Admin'],
                 role:null,
+                memberNameRules:[
+                  name => name && name.length >= 4 || "The Name most be more than 4 characters"
+                ],
+                memberEmailRules:[
+                  email => email && email.length >= 6 || "The Email most be more than 6 characters"
+                ],
+                memberPasswordRules:[
+                  password => password && password.length >= 8 || "The Password most be more than 8 characters"
+                ],
+                memberRoleRules:[
+                  role => role !== null || "Please Choose Role"
+                ],
             };
         },
         methods:{
             AddMember(){
+
+                // Check the form if it's not validate return null
+                if(!this.$refs.addMemberForm.validate()){
+                  return null;
+                }
+
                 let newMemberInfo = {
                     name: this.name,
                     email: this.email,
