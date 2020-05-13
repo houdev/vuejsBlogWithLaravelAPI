dee0a73c1bd00557c5ad2a002fbdb4404e8bc7e4
houssam boumaraf <houssambat@gmail.com>
2020-05-13 14:22:29 +0100

    style: Added new design to the articles list

--- spa/src/components/Articles.vue
+++ spa/src/components/Articles.vue
@@ -1,14 +1,16 @@
 <template>
-    <div>
-			<articles-list 
-			v-for="(myArticle, index) in myArticles" 
-			:key="index" 
-			:myArticle="myArticle"
-      :currentIndex="index"
-			@deleteArticle="$emit('deleteArticle' , myArticle.id)"
-      @updateArticle="(id, updatedArticle) => $emit('updateArticle', id, updatedArticle)"
-				/>
-    </div>
+	<v-container>
+		<v-row justify="center" align="center">
+			<articles-list
+				v-for="(myArticle, index) in myArticles"
+				:key="index"
+				:myArticle="myArticle"
+				:currentIndex="index"
+				@deleteArticle="$emit('deleteArticle' , myArticle.id)"
+				@updateArticle="(id, updatedArticle) => $emit('updateArticle', id, updatedArticle)"
+			/>
+		</v-row>
+	</v-container>
 </template>
 
 <script>
--- spa/src/components/ArticlesList.vue
+++ spa/src/components/ArticlesList.vue
@@ -1,167 +1,23 @@
 <template>
-    <div>
-      <v-container>
-        <v-row justify="center" align="center">
-          <v-card width="85%">
+    <v-col cols="4" align="center">
+      <v-card width="85%">
 
-            <v-list-item three-line :to="'/ViewArticle/'+myArticle.id">
-              <v-list-item-content>
-                <div class="overline mb-4">{{ myArticle.created_at }}</div>
-                <v-list-item-title  class="headline mb-1">{{ myArticle.title }}</v-list-item-title>
-                <v-list-item-subtitle>{{ myArticle.body }}</v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
+        <v-list-item three-line :to="'/ViewArticle/'+myArticle.id">
+          <v-list-item-content>
+            <div class="overline mb-4">{{ myArticle.created_at }}</div>
+            <v-list-item-title  class="headline mb-1">{{ myArticle.title }}</v-list-item-title>
+            <v-list-item-subtitle>{{ myArticle.body }}</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
 
-            <v-card-actions v-if="!hideActionButtonsFromHome">
-
-              <v-btn icon>
-                <v-icon
-                  color="error" @click="showDeleteDialog = true"
-                >
-                mdi-delete
-                </v-icon>
-              </v-btn>
-              <v-btn icon>
-                <v-icon
-                  color="blue"
-                  @click="showEditDialog = true"
-                >
-                mdi-pencil
-                </v-icon>
-              </v-btn>
-
-            </v-card-actions>
-
-          </v-card>
-        </v-row>
-      <v-dialog
-        v-model="showDeleteDialog"
-        max-width="290"
-      >
-        <v-card>
-          <v-card-title class="headline">Delete This Article!</v-card-title>
-
-          <v-card-text>
-            Do You Want To Delete This Article?
-          </v-card-text>
-
-          <v-card-actions>
-            <v-spacer></v-spacer>
-
-            <v-btn
-              color="green darken-1"
-              text
-              @click="showDeleteDialog = false"
-            >
-              No
-            </v-btn>
-
-            <v-btn
-              color="green darken-1"
-              text
-              @click="deleteArticle(myArticle.id)"
-            >
-              Yes
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-
-      <v-dialog
-        v-model="showEditDialog"
-        max-width="50%"
-      >
-        <v-card>
-          <v-card-title class="headline">Edit This Article</v-card-title>
-
-          <v-card-text>
-            <v-container>
-              <v-row>
-                <v-col cols="12">
-                  <v-text-field 
-                    label="Title" 
-                    v-model="currentTitle"
-                  >
-                  </v-text-field>
-                </v-col>
-                <v-col cols="12">
-                  <v-textarea
-                    v-model="currentBody"
-                    outlined
-                    name="bdoy"
-                    label="Bdoy"
-                  >
-                  </v-textarea>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-card-text>
-
-          <v-card-actions>
-            <v-spacer></v-spacer>
-
-            <v-btn
-              color="green darken-1"
-              text
-              @click="showEditDialog = false"
-            >
-              Cancel
-            </v-btn>
-
-            <v-btn
-              color="green darken-1"
-              text
-              @click="UpdateMyArticle(myArticle.id)"
-            >
-              Save
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-
-      </v-container>
-    </div>
+      </v-card>
+    </v-col>
 </template>
 
 <script>
 export default {
     name:'ArticlesList',
     props:['myArticle','currentIndex'],
-    data(){
-      return{
-        showEdit:false,
-        showEditDialog:false,
-        showDeleteDialog:false,
-        currentTitle:this.myArticle.title,
-        currentBody:this.myArticle.body,
-        hideActionButtonsFromHome:false,
-      }
-    },
-    methods:{
-      UpdateMyArticle(id){
-        let updatedArticle = {
-          title: this.currentTitle,
-          body: this.currentBody,
-        }
-        this.$emit('updateArticle', id, updatedArticle);
-
-        //close dialog
-        this.showEditDialog = false;
-      },
-      deleteArticle(myArticleId){
-        
-         this.$emit('deleteArticle' , myArticleId);
-
-         //close dialog
-         this.showDeleteDialog = false;
-        
-      },
-    },
-  created() {
-    if(this.$route.path == '/'){
-      this.hideActionButtonsFromHome = true;
-    }
-  }
 }
 </script>
 
